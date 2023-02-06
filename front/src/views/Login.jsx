75921c38e69f93c8d8afc72877a26107bb74c919
matham1201 <112947368+matham1201@users.noreply.github.com>
2023-02-06 17:54:44 +0100

    Page connexion finit (logo)

--- front/src/views/Login.jsx
+++ front/src/views/Login.jsx
@@ -1,9 +1,19 @@
 import Login from '../components/Login'
+import {useNavigate} from 'react-router-dom'
 
 function LoginPage() {
+  const navigate = useNavigate()
+  const ReceiveData = (mail,psw) =>{
+    console.log(mail)
+    console.log(psw)
+  }
+  
+  const IntoCustomerAccount = () => {
+    navigate('/')
+  }
 
   return (
-      <Login/>
+      <Login ReceiveData={ReceiveData} IntoCustomerAccount={IntoCustomerAccount}/>
   )
 }
 
