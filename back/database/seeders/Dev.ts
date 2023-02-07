9f433cdfe8c9a7af7989c21072e0c1e20b9baa7a
kevinguyodo <kevin.guyodo@ynov.com>
2023-02-07 18:16:00 +0100

    seeder created and finished

--- back/database/seeders/Dev.ts
+++ back/database/seeders/Dev.ts
@@ -10,105 +10,149 @@ import User from "App/Models/User";
 export default class extends BaseSeeder {
   public async run() {
     // Write your database queries inside the run method
-    const role = await Role.create({
-      name: "role name",
-    });
+    await Role.createMany([
+      {
+        name: "Administrateur",
+      },
+      {
+        name: "Commercial",
+      },
+      {
+        name: "Client",
+      },
+    ]);
 
-    const companyCategory = await CompanyCategory.create({
-      name: "name company category",
-    });
+    await CompanyCategory.createMany([
+      {
+        name: "Electroménager",
+      },
+      {
+        name: "Informatique",
+      },
+      {
+        name: "Alimentaire",
+      },
+      {
+        name: "Textile",
+      },
+      {
+        name: "Sport",
+      },
+      {
+        name: "Loisir",
+      },
+      {
+        name: "Educatif",
+      },
+    ]);
 
-    const user = await User.create({
-      name: "user name",
-      surname: "user surname",
-      email: "test@test.com",
-      address: "this is adresse test",
-      phone_number: "06 06 06 06 06",
-      password: " ",
-      role_id: role.id,
-    });
+    await User.createMany([
+      {
+        name: "eleve",
+        surname: "eleve",
+        email: "eleve@ynov.com",
+        address: "this is adresse test",
+        phone_number: "06 06 06 06 06",
+        password: "passwordeleve",
+        role_id: 1,
+      },
+      {
+        name: "prof",
+        surname: "prof",
+        email: "prof@ynov.com",
+        address: "adresse_prof",
+        phone_number: "06 06 06 06 06",
+        password: "prof_password",
+        role_id: 2,
+      },
+      {
+        name: "administration",
+        surname: "administration",
+        email: "administration@ynov.com",
+        address: "adresse_administration",
+        phone_number: "06 06 06 06 06",
+        password: "administration_password",
+        role_id: 3,
+      },
+    ]);
 
-    const category = await Category.create({
-      name: "category test",
-    });
+    await Category.createMany([
+      {
+        name: "Téléphone",
+      },
+      {
+        name: "Chaussure",
+      },
+      {
+        name: "Vêtement",
+      },
+      {
+        name: "Télévision",
+      },
+      {
+        name: "Conserve",
+      },
+      {
+        name: "Livre",
+      },
+      {
+        name: "Vélo",
+      },
+    ]);
 
-    const cart = await Cart.create({
-      client_id: user.id,
-    });
+    await Cart.createMany([
+      {
+        client_id: 1,
+      },
+      {
+        client_id: 2,
+      },
+      {
+        client_id: 3,
+      },
+    ]);
 
-    await Company.create({
-      name: "name company",
-      phone_number: "06 06 06 06 06",
-      email: "test@test.com",
-      address: "this is adresse",
-      user_id: user.id,
-      company_category_id: companyCategory.id,
-    });
+    await Company.createMany([
+      {
+        name: "Decathlon",
+        phone_number: "06 06 06 06 06",
+        email: "decathlon@decathlon.com",
+        address: "decathlon adress",
+        user_id: 1,
+        company_category_id: 7,
+      },
+      {
+        name: "Huawai",
+        phone_number: "06 06 06 06 06",
+        email: "huawai@huawai.com",
+        address: "huawai adress",
+        user_id: 2,
+        company_category_id: 2,
+      },
+    ]);
 
-    await Product.create({
-      name: "product name1",
-      description: "description1",
-      price: 1501,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    await Product.create({
-      name: "product name2",
-      description: "description2",
-      price: 1502,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    await Product.create({
-      name: "product name3",
-      description: "description3",
-      price: 1503,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    await Product.create({
-      name: "product name4",
-      description: "description4",
-      price: 1504,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    await Product.create({
-      name: "product name5",
-      description: "description5",
-      price: 1505,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    await Product.create({
-      name: "product name6",
-      description: "description6",
-      price: 1506,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    await Product.create({
-      name: "product name7",
-      description: "description7",
-      price: 1507,
-      category_id: category.id,
-      cart_id: cart.id,
-    });
-
-    // const user = await User.create({
-    //   email: "nathan.schneider@ynov.com",
-    //   firstname: "nathan",
-    //   lastname: "schneider",
-    //   username: "La manette",
-    //   password: "Le schneid",
-    //   phone_number: "06 06 06 06 06",
-    //   role: "membre",
-    // });
+    await Product.createMany([
+      {
+        name: "Huawai pro",
+        description: "Téléphone performant",
+        price: 1000,
+        category_id: 1,
+        cart_id: 1,
+      },
+      {
+        name: "Les misérables",
+        description: "Très bon livre, un classique",
+        price: 20,
+        category_id: 6,
+        cart_id: 2,
+      },
+      {
+        name: "BMX",
+        description: "Bon vélo pour débuter",
+        price: 1500,
+        category_id: 7,
+        cart_id: 3,
+      },
+    ]);
   }
 }
