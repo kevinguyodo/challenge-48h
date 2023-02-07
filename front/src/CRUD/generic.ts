becf6e24aabef3cca3857a501b38c75e5f404589
kevinguyodo <kevin.guyodo@ynov.com>
2023-02-07 04:08:25 +0100

    try to connect api to fron

---
+++ front/src/CRUD/generic.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+// TODO Change by your IP address
+const baseURL: string = "http://10.44.17.132:3333/";
+
+export const getData = (endPoint: string) => {
+  try {
+    const response = axios.get(baseURL + endPoint, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }); //use data destructuring to get data from the promise object
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const postData = (endPoint: string, newData: object) => {
+  try {
+    const response = axios.post(baseURL + endPoint, newData);
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+export const putData = (
+  endPoint: string,
+  URLParameter: string,
+  newData: object
+) => {
+  try {
+    const response = axios.put(baseURL + endPoint + URLParameter, newData);
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
--- front/src/views/MainPage.jsx
+++ front/src/views/MainPage.jsx
@@ -1,22 +1,28 @@
-import Page from '../components/MainPage'
-import { useNavigate } from 'react-router-dom';
+import Page from "../components/MainPage";
+import { useNavigate } from "react-router-dom";
+import { getData } from "../CRUD/generic";
 function Mainpage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const test = getData("company");
+  console.log(test);
 
   const ToLogin = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
   const ToSellerCreateAccount = () => {
-    navigate('CustomerCreateAccount')
-  }
+    navigate("CustomerCreateAccount");
+  };
   const ToCustomerCreateAccount = () => {
-    navigate('CustomerCreateAccount')
-  }
+    navigate("CustomerCreateAccount");
+  };
 
   return (
-      <Page ToLogin={ToLogin} ToSellerCreateAccount={ToSellerCreateAccount} ToCustomerCreateAccount={ToCustomerCreateAccount}/>
-  )
+    <Page
+      ToLogin={ToLogin}
+      ToSellerCreateAccount={ToSellerCreateAccount}
+      ToCustomerCreateAccount={ToCustomerCreateAccount}
+    />
+  );
 }
 
-export default Mainpage
-
+export default Mainpage;
